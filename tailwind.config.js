236065e0715662f04572245c503cf776d5a17422
korneliafrankiewicz <nelcia6991@gmail.com>
2022-02-11 15:25:24 +0100

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // {
+    //   resolve: 'gatsby-plugin-sass',
+    //   options: {
+    //     postCssPlugins: [
+    //       require("tailwindcss"),
+    //       require("./tailwind.config.js"),
+    //     ],
+    //   },
+    // },
+  ],
+}
